60ae2867e1a2c8d03bd593e2497010142733f238
Luca GORTANA <lgortana@node121.cluster-fon.interne.mines-paristech.fr>
2026-01-27 17:57:17 +0100

    changed default value for normalization

--- hedest/main.py
+++ hedest/main.py
@@ -47,7 +47,7 @@ def main(
     spot_dict_file: Optional[str] = typer.Option(None, help="Path to the spot-to-cell json file."),
     model_name: str = typer.Option("default", help="Type of model. Can be 'default', 'convnet', or 'resnet18'."),
     hidden_dims: str = typer.Option("512,256", help="Hidden dimensions for the model (comma-separated)."),
-    norm: bool = typer.Option(True, help="Whether to add a LayerNorm layer."),
+    norm: bool = typer.Option(False, help="Whether to add a LayerNorm layer."),
     dropout: float = typer.Option(0.0, help="Dropout rate."),
     batch_size: int = typer.Option(64, help="Batch size for model training."),
     lr: float = typer.Option(0.0001, help="Learning rate."),
