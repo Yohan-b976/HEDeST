c92e3aaac48c67b9f05052d5e660d907313d2d48
Yohan <yohanbeaumatin@gmail.com>
2026-04-09 14:07:28 +0200

    adapt to my env

--- run_hedest.sh
+++ run_hedest.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 
 #SBATCH --job-name=HEDeST
-#SBATCH --output=/cluster/CBIO/home/lgortana/HEDeST/log/hedest_%j.log
-#SBATCH --error=/cluster/CBIO/home/lgortana/HEDeST/log/hedest_%j.err
+#SBATCH --output=/cluster/CBIO/home/ybeaumatin/HEDeST/log/hedest_%j.log
+#SBATCH --error=/cluster/CBIO/home/ybeaumatin/HEDeST/log/hedest_%j.err
 #SBATCH --gres=gpu:1
 #SBATCH -p cbio-gpu
 #SBATCH --cpus-per-task=8
 
 echo 'Found a place!'
 
-source /cluster/CBIO/home/lgortana/anaconda3/etc/profile.d/conda.sh
-conda activate plugin-env
+source /cluster/CBIO/home/ybeaumatin/miniconda3/etc/profile.d/conda.sh
+conda activate hedest-env
 
-export LD_LIBRARY_PATH=/cluster/CBIO/home/lgortana/anaconda3/envs/plugin-env/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/cluster/CBIO/home/ybeaumatin/miniconda3/envs/hedest-env/lib:$LD_LIBRARY_PATH
 
-for seed in {0..9}; do
-  python3 -u hedest/main.py \
-    /cluster/CBIO/data1/lgortana/Visium_FFPE_Human_Breast_Cancer/moco_embed_BRCA_64px_20um.pt \
-    case_study/DestVI_BRCA_prop.csv \
-    --json-path /cluster/CBIO/data1/lgortana/Visium_FFPE_Human_Breast_Cancer/seg_json/pannuke_fast_mask_lvl3.json \
-    --path-st-adata /cluster/CBIO/data1/lgortana/Visium_FFPE_Human_Breast_Cancer/adata.h5ad \
-    --adata-name Visium_FFPE_Human_Breast_Cancer \
-    --spot-dict-file /cluster/CBIO/data1/lgortana/Visium_FFPE_Human_Breast_Cancer/spot_dict.json \
-    --model-name default \
-    --norm \
-    --dropout 0.0 \
-    --batch-size 64 \
-    --lr 1e-4 \
-    --divergence l2 \
-    --alpha 0 \
-    --beta 0.0 \
-    --epochs 100 \
-    --train-size 0.8 \
-    --val-size 0.1 \
-    --out-dir models/BRCA-test/DestVI_4000_hvg_squash_06_02_large/model_default_hidden_dim_512-256_norm_True_dropout_0.0_alpha_0.0_lr_0.0001_divergence_l2_beta_0.0/seed_${seed} \
-    --save-geojson \
-    --rs $seed
-done
+
+python3 -u hedest/main.py \
+  /cluster/CBIO/data1/ybeaumatin/Visium_FFPE_Human_Breast_Cancer/moco_embed_BRCA_64px_20um.pt \
+  case_study/DestVI_BRCA_prop.csv \
+  --json-path /cluster/CBIO/data1/ybeaumatin/Visium_FFPE_Human_Breast_Cancer/seg_json/pannuke_fast_mask_lvl3.json \
+  --path-st-adata /cluster/CBIO/data1/ybeaumatin/Visium_FFPE_Human_Breast_Cancer/adata.h5ad \
+  --adata-name Visium_FFPE_Human_Breast_Cancer \
+  --spot-dict-file /cluster/CBIO/data1/ybeaumatin/Visium_FFPE_Human_Breast_Cancer/spot_dict.json \
+  --model-name default \
+  --norm \
+  --dropout 0.0 \
+  --batch-size 64 \
+  --lr 1e-4 \
+  --divergence l2 \
+  --alpha 0 \
+  --beta 0.0 \
+  --epochs 100 \
+  --train-size 0.8 \
+  --val-size 0.1 \
+  --out-dir models/BRCA-test/DestVI_4000_hvg_squash_06_02_large/model_default_hidden_dim_512-256_norm_True_dropout_0.0_alpha_0.0_lr_0.0001_divergence_l2_beta_0.0 \
+  --save-geojson \
+  --rs 42
+
